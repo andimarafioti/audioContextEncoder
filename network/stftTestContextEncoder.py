e8ef51ab2142f394988aeb553541694e54939691
amarafioti <andimarafioti@gmail.com>
2018-03-01 10:54:09 +0100

    up

--- network/stftTestContextEncoder.py
+++ network/stftTestContextEncoder.py
@@ -9,11 +9,19 @@ __author__ = 'Andres'
 class StftTestContextEncoder(ContextEncoderNetwork):
     def _loss_graph(self):
         with tf.variable_scope("Loss"):
+            sides = self._model.input()
+            signal_length = self._window_size - self._gap_length
+            first_half = sides[:, :signal_length // 2]
+            second_half = sides[:, signal_length // 2:]
+
+            reconstructed_signal = tf.concat([first_half, self.gap_data, second_half], axis=1)
+
             fft_frame_length = 512
             fft_frame_step = 128
-            stft = tf.contrib.signal.stft(signals=self.gap_data, frame_length=fft_frame_length,
+            stft = tf.contrib.signal.stft(signals=reconstructed_signal, frame_length=fft_frame_length,
                                           frame_step=fft_frame_step)
-            mag_stft = tf.abs(stft)
+            gap_stft = stft[:, 15:15+7, :]
+            mag_stft = tf.abs(gap_stft)
 
             # fft_unique_bins = fft_frame_length // 2 + 1  # 257
             # num_ffts = int((self._gap_length - fft_frame_length) / fft_frame_step) + 1  # 5
@@ -44,21 +52,28 @@ class StftTestContextEncoder(ContextEncoderNetwork):
                 print(batch_num)
                 print("rec End of queue!")
                 break
+            signal_length = self._window_size - self._gap_length
+            first_half = sides[:, :signal_length // 2]
+            second_half = sides[:, signal_length // 2:]
+
+            reconstructed_signal = tf.concat([first_half, gaps, second_half], axis=1)
+
             fft_frame_length = 512
             fft_frame_step = 128
-            stft = tf.contrib.signal.stft(signals=gaps, frame_length=fft_frame_length,
+            stft = tf.contrib.signal.stft(signals=reconstructed_signal, frame_length=fft_frame_length,
                                           frame_step=fft_frame_step)
-            mag_stft = tf.abs(stft)
+            gap_stft = stft[:, 15:15+7, :]
+            mag_stft = tf.abs(gap_stft)
 
             feed_dict = {self._model.input(): sides, self.gap_data: gaps}
             reconstructed_input, original = sess.run([self._reconstructed_input_data, mag_stft], feed_dict=feed_dict)
             out_gaps.append(np.reshape(original, (-1)))
             reconstructed.append(np.reshape(reconstructed_input, (-1)))
         reconstructed = reconstructed.finalize()
-        reconstructed = np.reshape(reconstructed, (-1, 5, 257))
+        reconstructed = np.reshape(reconstructed, (-1, 7, 257))
 
         out_gaps = out_gaps.finalize()
-        out_gaps = np.reshape(out_gaps, (-1, 5, 257))
+        out_gaps = np.reshape(out_gaps, (-1, 7, 257))
         data_reader.finish()
 
         return reconstructed, out_gaps
