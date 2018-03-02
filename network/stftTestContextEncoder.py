4c400abc7a59b075f5d8389a7e12345eef06dc03
amarafioti <andimarafioti@gmail.com>
2018-03-02 10:42:13 +0100

    network working with magnitude only

--- network/stftTestContextEncoder.py
+++ network/stftTestContextEncoder.py
@@ -1,26 +1,35 @@
 import tensorflow as tf
 import numpy as np
 from network.contextEncoder import ContextEncoderNetwork
+from utils.evaluationWriter import EvaluationWriter
+from utils.plotSummary import PlotSummary
 from utils.strechableNumpyArray import StrechableNumpyArray
+from utils.tfReader import TFReader
 
 __author__ = 'Andres'
 
 
 class StftTestContextEncoder(ContextEncoderNetwork):
+    def __init__(self, model, batch_size, stft, window_size, gap_length, learning_rate, name):
+        self._stft = stft
+        super(StftTestContextEncoder, self).__init__(model, batch_size, window_size, gap_length, learning_rate, name)
+        self._specgram = tf.placeholder(tf.float32, shape=(batch_size*8, 7, 257), name='specgram')
+        self._reconstructedAudio = self._computeIstftOnSpectogram(self._specgram, 512, 128)
+
+        self._sides = tf.placeholder(tf.float32, shape=(batch_size, self._window_size - self._gap_length), name='sides')
+        self._reconstructedSignal = self._reconstructSignal(self._sides, self.gap_data)
+
+    def _reconstructSignal(self, sides, gaps):
+        signal_length = self._window_size - self._gap_length
+        first_half = sides[:, :signal_length // 2]
+        second_half = sides[:, signal_length // 2:]
+
+        reconstructed_signal = tf.concat([first_half, gaps, second_half], axis=1)
+        return reconstructed_signal
+
     def _loss_graph(self):
         with tf.variable_scope("Loss"):
-            sides = self._model.input()
-            signal_length = self._window_size - self._gap_length
-            first_half = sides[:, :signal_length // 2]
-            second_half = sides[:, signal_length // 2:]
-
-            reconstructed_signal = tf.concat([first_half, self.gap_data, second_half], axis=1)
-
-            fft_frame_length = 512
-            fft_frame_step = 128
-            stft = tf.contrib.signal.stft(signals=reconstructed_signal, frame_length=fft_frame_length,
-                                          frame_step=fft_frame_step)
-            gap_stft = stft[:, 15:15+7, :]
+            gap_stft = self._stft[:, 15:15+7, :]
             mag_stft = tf.abs(gap_stft)
 
             # fft_unique_bins = fft_frame_length // 2 + 1  # 257
@@ -30,14 +39,16 @@ class StftTestContextEncoder(ContextEncoderNetwork):
 
             error = mag_stft - self._reconstructed_input_data
             reconstruction_loss = 0.5 * tf.reduce_sum(tf.reduce_sum(tf.square(error), axis=1))  # * (1 + 1 / norm_orig))
-            tf.summary.scalar("reconstruction_loss", reconstruction_loss)
+            rec_loss_summary = tf.summary.scalar("reconstruction_loss", reconstruction_loss)
 
             trainable_vars = tf.trainable_variables()
             lossL2 = tf.add_n([tf.nn.l2_loss(v) for v in trainable_vars if 'bias' not in v.name]) * 1e-2
-            tf.summary.scalar("lossL2", lossL2)
+            l2_loss_summary = tf.summary.scalar("lossL2", lossL2)
 
             total_loss = tf.add_n([reconstruction_loss, lossL2])
-            tf.summary.scalar("total_loss", total_loss)
+            total_loss_summary = tf.summary.scalar("total_loss", total_loss)
+
+            self._lossSummaries = tf.summary.merge([rec_loss_summary, l2_loss_summary, total_loss_summary])
 
             return total_loss
 
@@ -56,25 +67,114 @@ class StftTestContextEncoder(ContextEncoderNetwork):
             first_half = sides[:, :signal_length // 2]
             second_half = sides[:, signal_length // 2:]
 
-            reconstructed_signal = tf.concat([first_half, gaps, second_half], axis=1)
+            reconstructed_signal = np.concatenate([first_half, gaps, second_half], axis=1)
 
-            fft_frame_length = 512
-            fft_frame_step = 128
-            stft = tf.contrib.signal.stft(signals=reconstructed_signal, frame_length=fft_frame_length,
-                                          frame_step=fft_frame_step)
-            gap_stft = stft[:, 15:15+7, :]
-            mag_stft = tf.abs(gap_stft)
+            gap_stft = self._stft[:, 15:15+7, :]
 
-            feed_dict = {self._model.input(): sides, self.gap_data: gaps}
-            reconstructed_input, original = sess.run([self._reconstructed_input_data, mag_stft], feed_dict=feed_dict)
+            feed_dict = {self._model.input(): reconstructed_signal, self._model.isTraining(): False}
+            reconstructed_input, original = sess.run([self._reconstructed_input_data, gap_stft], feed_dict=feed_dict)
             out_gaps.append(np.reshape(original, (-1)))
             reconstructed.append(np.reshape(reconstructed_input, (-1)))
+
         reconstructed = reconstructed.finalize()
         reconstructed = np.reshape(reconstructed, (-1, 7, 257))
-
         out_gaps = out_gaps.finalize()
         out_gaps = np.reshape(out_gaps, (-1, 7, 257))
+
+        istft_original = sess.run(self._reconstructedAudio, feed_dict={self._specgram: out_gaps})
+        istft_reconstructed = sess.run(self._reconstructedAudio, feed_dict={self._specgram: reconstructed})
         data_reader.finish()
 
-        return reconstructed, out_gaps
+        return istft_reconstructed, istft_original
+
+    def _computeIstftOnSpectogram(self, spectogram, fft_frame_length, fft_frame_step):
+        complex_original = tf.complex(tf.abs(spectogram), tf.zeros(spectogram.shape))
+        istft_original = tf.contrib.signal.inverse_stft(stfts=complex_original,
+                                                        frame_length=fft_frame_length, frame_step=fft_frame_step)
+        return istft_original
+
+    def train(self, train_data_path, valid_data_path, num_steps=2e2, restore_num=None, per_process_gpu_memory_fraction=1):
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=per_process_gpu_memory_fraction)
+        with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+            try:
+                trainReader = TFReader(train_data_path, self._window_size, self._gap_length, capacity=int(2e5), num_epochs=400)
+                validReader = TFReader(valid_data_path, self._window_size, self._gap_length, capacity=int(2e5), num_epochs=40000)
+
+                saver = tf.train.Saver(max_to_keep=1000)
+                if restore_num:
+                    path = self.modelsPath(restore_num)
+                    self._initial_model_num = restore_num
+                    saver.restore(sess, path)
+                    sess.run([tf.local_variables_initializer()])
+                    print("Model restored.")
+                else:
+                    init = tf.global_variables_initializer()
+                    sess.run([init, tf.local_variables_initializer()])
+                    print("Initialized")
+
+                logs_path = '../logdir_real_cae/' + self._name  # write each run to a diff folder.
+                print("logs path:", logs_path)
+                writer = tf.summary.FileWriter(logs_path, graph=tf.get_default_graph())
+
+                train_SNR = tf.placeholder(tf.float32, name="train_SNR")
+                train_SNR_summary = tf.summary.scalar("training_SNR", train_SNR)
+                valid_SNR = tf.placeholder(tf.float32, name="valid_SNR")
+                valid_SNR_summary = tf.summary.scalar("validation_SNR", valid_SNR)
+                plot_summary = PlotSummary('reconstruction')
+
+                trainReader.start()
+                evalWriter = EvaluationWriter(self._name + '.xlsx')
+
+                # options = tf.RunOptions(trace_level=tf.RunOptions.FULL_TRACE)
+                # run_metadata = tf.RunMetadata()
+                # many_runs_timeline = TimeLiner()
+
+                for step in range(1, int(num_steps)):
+                    try:
+                        sides, gaps = trainReader.dataOperation(session=sess)
+                    except StopIteration:
+                        print(step)
+                        print("End of queue!")
+                        break
+
+                    rec = sess.run(self._reconstructedSignal, feed_dict={self._sides: sides, self.gap_data: gaps})
+
+                    feed_dict = {self._model.input(): rec, self.gap_data: gaps, self._model.isTraining(): True}
+                    sess.run(self._optimizer, feed_dict=feed_dict)  # , options=options, run_metadata=run_metadata)
+
+                    # fetched_timeline = timeline.Timeline(run_metadata.step_stats)
+                    # chrome_trace = fetched_timeline.generate_chrome_trace_format()
+                    # many_runs_timeline.update_timeline(chrome_trace)
+
+                    if step % 40 == 0:
+                        train_summ = sess.run(self._lossSummaries, feed_dict=feed_dict)
+                        writer.add_summary(train_summ, self._initial_model_num + step)
+                    if step % 1 == 0:
+                        print(step)
+                        reconstructed, out_gaps = self._reconstruct(sess, trainReader, max_steps=8)
+                        plot_summary.plotSideBySide(out_gaps, reconstructed)
+                        train_SNRs = tf.reduce_mean(self._pavlovs_SNR(out_gaps, reconstructed))
+                        step_train_SNR = sess.run(train_SNRs)
+                        trainSNRSummaryToWrite = sess.run(train_SNR_summary, feed_dict={train_SNR: step_train_SNR})
+                        writer.add_summary(trainSNRSummaryToWrite, self._initial_model_num + step)
+                        summaryToWrite = plot_summary.produceSummaryToWrite(sess)
+                        writer.add_summary(summaryToWrite, self._initial_model_num + step)
+                        saver.save(sess, self.modelsPath(self._initial_model_num + step))
+                        reconstructed, out_gaps = self._reconstruct(sess, validReader, max_steps=8)
+                        step_valid_SNR = evalWriter.evaluate(reconstructed, out_gaps, self._initial_model_num + step)
+                        validSNRSummaryToWrite = sess.run(valid_SNR_summary, feed_dict={valid_SNR: step_valid_SNR})
+                        writer.add_summary(validSNRSummaryToWrite, self._initial_model_num + step)
+
+            except KeyboardInterrupt:
+                pass
+            # many_runs_timeline.save('timeline_03_merged_%d_runs.json' % step)
+            evalWriter.save()
+            train_summ = sess.run(self._lossSummaries, feed_dict=feed_dict)
+            writer.add_summary(train_summ, self._initial_model_num + step)
+            saver.save(sess, self.modelsPath(self._initial_model_num + step))
+            self._initial_model_num += step
+
+            trainReader.finish()
+            print("Finalizing at step:", self._initial_model_num)
+            print("Last saved model:", self.modelsPath(self._initial_model_num))
 
---
+++ simulations/runNatStftMarchTest.py
@@ -0,0 +1,62 @@
+import tensorflow as tf
+
+from network.sequentialModel import SequentialModel
+from network.stftTestContextEncoder import StftTestContextEncoder
+
+__author__ = 'Andres'
+
+tf.reset_default_graph()
+train_filename = '../test_w5120_g1024_h512_ex63501.tfrecords'
+valid_filename = '../test_w5120_g1024_h512_ex63501.tfrecords'
+
+window_size = 5120
+gap_length = 1024
+batch_size = 256
+
+aModel = SequentialModel(shapeOfInput=(batch_size, window_size), name="context encoder")
+
+signal = aModel.output()
+
+with tf.name_scope('Energy_Spectogram'):
+    fft_frame_length = 512
+    fft_frame_step = 128
+    stft = tf.contrib.signal.stft(signals=signal, frame_length=fft_frame_length, frame_step=fft_frame_step)
+
+    sides_stft = tf.stack((stft[:, :15, :], stft[:, 15+7:, :]), axis=3)
+    print(sides_stft)
+
+    mag_stft = tf.abs(sides_stft)    # (256, 15, 257, 2)
+    mag_stft = tf.reshape(mag_stft, (batch_size, 15, 257, 2))
+    aModel.setOutputTo(mag_stft)
+	
+with tf.variable_scope("Encoder"):
+    filter_widths = [(3, 33), (2, 9), (1, 3)]
+    input_channels = [2, 32, 64]
+    output_channels = [32, 64, 128]
+    strides = [[1, 2, 4, 1], [1, 2, 4, 1], [1, 2, 4, 1]]
+    names = ['First_Conv', 'Second_Conv', 'Third_Conv']
+    aModel.addSeveralConvLayers(filter_shapes=filter_widths, input_channels=input_channels,
+                                output_channels=output_channels, strides=strides, names=names)
+
+aModel.addReshape((batch_size, 1280))
+aModel.addFullyConnectedLayer(1280, 896, 'Fully')
+aModel.addRelu()
+aModel.addReshape((batch_size, 1, 7, 128))
+
+with tf.variable_scope("Decoder"):
+    filter_widths = [(1, 5), (1, 9)]
+    input_channels = [128, 256]
+    output_channels = [256, 128]
+    strides = [[1, 1, 2, 1]] * len(input_channels)
+    names = ['First_Deconv', 'Second_Deconv']
+    aModel.addSeveralDeconvLayers(filter_shapes=filter_widths, input_channels=input_channels,
+                                  output_channels=output_channels, strides=strides, names=names)
+    aModel.addReshape((batch_size, 1, 7, 512))
+    aModel.addDeconvLayerWithoutNonLin(filter_shape=(1, 3), input_channels=512, output_channels=257,
+                                       stride=(1, 1, 1, 1), name="Last_Deconv")
+    aModel.addReshape((batch_size, 7, 257))
+
+print(aModel.description())
+aContextEncoderNetwork = StftTestContextEncoder(model=aModel, batch_size=batch_size, stft=stft, window_size=window_size,
+                                               gap_length=gap_length, learning_rate=1e-4, name='nat_mag_stft_3_')
+aContextEncoderNetwork.train(train_filename, valid_filename, num_steps=1e6)
