ce53379282cc227aca2754631bfca969a7c6fada
amarafioti <andimarafioti@gmail.com>
2018-03-02 12:14:07 +0100

    Improved hyper-parameter selection

--- simulations/runNatStftMarchTest.py
+++ simulations/runNatStftMarchTest.py
@@ -28,33 +28,39 @@ with tf.name_scope('Energy_Spectogram'):
     aModel.setOutputTo(mag_stft)
 	
 with tf.variable_scope("Encoder"):
-    filter_shapes = [(3, 33), (2, 9), (1, 3)]
-    input_channels = [2, 32, 64]
-    output_channels = [32, 64, 128]
-    strides = [[1, 2, 4, 1], [1, 2, 4, 1], [1, 2, 4, 1]]
-    names = ['First_Conv', 'Second_Conv', 'Third_Conv']
+    filter_shapes = [(7, 89), (3, 17), (2, 6), (1, 5), (1, 3)]
+    input_channels = [2, 32, 64, 128, 128]
+    output_channels = [32, 64, 128, 128, 200]
+    strides = [[1, 2, 2, 1], [1, 2, 3, 1], [1, 2, 3, 1], [1, 1, 2, 1], [1, 1, 1, 1]]
+    names = ['First_Conv', 'Second_Conv', 'Third_Conv', 'Fourth_Conv', 'Fifth_Conv']
     aModel.addSeveralConvLayers(filter_shapes=filter_shapes, input_channels=input_channels,
                                 output_channels=output_channels, strides=strides, names=names)
 
-aModel.addReshape((batch_size, 1280))
-aModel.addFullyConnectedLayer(1280, 896, 'Fully')
+aModel.addReshape((batch_size, 3200))
+aModel.addFullyConnectedLayer(3200, 1799, 'Fully')
 aModel.addRelu()
-aModel.addReshape((batch_size, 1, 7, 128))
+aModel.addReshape((batch_size, 7, 257, 1))
 
 with tf.variable_scope("Decoder"):
-    filter_shapes = [(1, 5), (1, 9)]
-    input_channels = [128, 256]
-    output_channels = [256, 128]
-    strides = [[1, 1, 2, 1]] * len(input_channels)
+    filter_shapes = [(5, 189), (3, 33)]
+    input_channels = [1, 64]
+    output_channels = [64, 16]
+    strides = [[1, 2, 2, 1]] * len(input_channels)
     names = ['First_Deconv', 'Second_Deconv']
     aModel.addSeveralDeconvLayers(filter_shapes=filter_shapes, input_channels=input_channels,
                                   output_channels=output_channels, strides=strides, names=names)
-    aModel.addReshape((batch_size, 1, 7, 512))
-    aModel.addDeconvLayerWithoutNonLin(filter_shape=(1, 3), input_channels=512, output_channels=257,
+
+    aModel.addReshape((batch_size, 7, 257, 256))
+    aModel.addDeconvLayer(filter_shape=(3, 33), input_channels=256, output_channels=4, stride=(1, 2, 2, 1),
+                          name='Third_deconv')
+
+    aModel.addReshape((batch_size, 7, 257, 16))
+
+    aModel.addDeconvLayerWithoutNonLin(filter_shape=(7, 257), input_channels=16, output_channels=1,
                                        stride=(1, 1, 1, 1), name="Last_Deconv")
     aModel.addReshape((batch_size, 7, 257))
 
 print(aModel.description())
 aContextEncoderNetwork = StftTestContextEncoder(model=aModel, batch_size=batch_size, stft=stft, window_size=window_size,
-                                               gap_length=gap_length, learning_rate=1e-4, name='nat_mag_stft_3_')
+                                               gap_length=gap_length, learning_rate=1e-4, name='nat_mag_stft_5_')
 aContextEncoderNetwork.train(train_filename, valid_filename, num_steps=1e6)
