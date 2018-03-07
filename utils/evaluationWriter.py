53748291e52c3cd07022b0903de597d94f18c7bd
amarafioti <andimarafioti@gmail.com>
2018-03-07 14:59:30 +0100

    Evaluation writer adapted to 2D

--- utils/evaluationWriter.py
+++ utils/evaluationWriter.py
@@ -23,14 +23,28 @@ class EvaluationWriter(object):
         self._index += 3
         return np.mean(SNRs)
 
-    def _pavlovs_SNR(self, y_orig, y_inp):
-        norm_y_orig = self._squaredEuclideanNorm(y_orig)
-        norm_y_orig_minus_y_inp = self._squaredEuclideanNorm(y_orig - y_inp)
+    def evaluateImages(self, reconstructed, original_gaps, step):
+        assert (original_gaps.shape == reconstructed.shape)
+
+        SNRs = self._pavlovs_SNR(original_gaps, reconstructed, onAxis=(1, 2))
+
+        norm_orig = self._squaredEuclideanNorm(original_gaps) / 5
+        error = original_gaps - reconstructed
+        reconstruction_loss = 0.5 * np.sum(np.square(error), axis=1) * (1 + 1 / norm_orig)
+
+        df = pd.DataFrame({'SNRs ' + str(step): SNRs, 'reconstruction_loss ' + str(step): reconstruction_loss})
+        df.describe().to_excel(self._writer, sheet_name='general', startcol=self._index, index=not self._index)
+        self._index += 3
+        return np.mean(SNRs)
+
+    def _pavlovs_SNR(self, y_orig, y_inp, onAxis=(1,)):
+        norm_y_orig = self._squaredEuclideanNorm(y_orig, onAxis)
+        norm_y_orig_minus_y_inp = self._squaredEuclideanNorm(y_orig - y_inp, onAxis)
         return 10 * np.log10(norm_y_orig / norm_y_orig_minus_y_inp)
 
-    def _squaredEuclideanNorm(self, vector):
+    def _squaredEuclideanNorm(self, vector, onAxis=(1,)):
         squared = np.square(vector)
-        summed = np.sum(squared, axis=1)
+        summed = np.sum(squared, axis=onAxis)
         return summed
 
     def save(self):
