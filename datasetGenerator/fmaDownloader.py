28c2546ddff84fe4f7560d4a198f2e8a27ce8e9e
amarafioti <andimarafioti@gmail.com>
2018-04-10 12:42:03 +0200

    Added an FMA downloader to get this dataset

---
+++ datasetGenerator/fmaDownloader.py
@@ -0,0 +1,13 @@
+from datasetGenerator.downloader import Downloader
+
+__author__ = 'Andres'
+
+
+class FMADownloader(Downloader):
+    SMALL_LINK = 'https://os.unil.cloud.switch.ch/fma/fma_small.zip'
+    SMALL_FILENAME = 'fma_small.zip'
+
+    def downloadAndExtract(self):
+        self._download(self.SMALL_LINK, self.SMALL_FILENAME)
+        self._extractZip(self.SMALL_FILENAME)
+
