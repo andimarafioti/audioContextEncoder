2cee8201f769504b12920ea7b83285b89d3bc466
amarafioti <andimarafioti@gmail.com>
2018-04-10 12:41:44 +0200

    Added a downloader class to get files and extract them.

---
+++ datasetGenerator/downloader.py
@@ -0,0 +1,36 @@
+import ssl
+import urllib.request
+import tarfile
+import zipfile
+
+__author__ = 'Andres'
+
+
+class Downloader(object):
+    def _download(self, aLink, toAFilename):
+        print("Downloading to ", toAFilename)
+        size = 0
+        blocksize = 4096
+
+        with urllib.request.urlopen(aLink, context=ssl.SSLContext(ssl.PROTOCOL_TLSv1)) as response, \
+                open(toAFilename, 'wb') as out_file:  # context avoids SSL certifications
+            length = float(response.getheader('content-length'))
+            data = response.read(blocksize)
+            out_file.write(data)
+            while data:
+                size += len(data)
+                print('\r Downloaded {:.2f} % '.format(100 * size / length), end='')
+                data = response.read(blocksize)
+                out_file.write(data)
+
+    def _extractTar(self, aFile):
+        print('\n extracting ', aFile)
+        tar = tarfile.open(aFile)
+        tar.extractall()
+        tar.close()
+
+    def _extractZip(self, aFile):
+        print('\n extracting ', aFile)
+        zip_ref = zipfile.ZipFile(aFile, 'r')
+        zip_ref.extractall()
+        zip_ref.close()
