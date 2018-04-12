156fd56392474da53b61b09959011f558f73c025
amarafioti <andimarafioti@gmail.com>
2018-04-12 11:05:13 +0200

    Refactored the declaration of the directories to be consistent with the NSynthDownloader

--- datasetGenerator/fmaDownloader.py
+++ datasetGenerator/fmaDownloader.py
@@ -9,7 +9,10 @@ class FMADownloader(Downloader):
     SMALL_LINK = 'https://os.unil.cloud.switch.ch/fma/fma_small.zip'
     SMALL_FILENAME = 'fma_small.zip'
     SMALL_DIR = SMALL_FILENAME[:-4]
-    DIRS = {'train': 'FMA-train', 'valid': 'FMA-valid', 'test': 'FMA-test'}
+
+    TRAIN_DIR = 'FMA-train'
+    VALID_DIR = 'FMA-valid'
+    TEST_DIR = 'FMA-test'
 
     def _downloadLinksAndFilenames(self):
         return [(self.SMALL_LINK, self.SMALL_FILENAME)]
@@ -19,9 +22,9 @@ class FMADownloader(Downloader):
 
     def _divideDataIntoTrainValidAndTestSubsets(self):
         print('Dividing FMA dataset into training, validation and testing subsets.')
-        for dirKey in self.DIRS:
+        for dir_name in [self.TRAIN_DIR, self.VALID_DIR, self.TEST_DIR]:
             try:
-                os.mkdir(self.DIRS[dirKey])
+                os.mkdir(dir_name)
             except FileExistsError as e:
                 print('Directory already existed, proceed with caution.\nException:', e)
 
@@ -30,13 +33,13 @@ class FMADownloader(Downloader):
             for file_name in file_names:
                 i += 1
                 if i < 7:
-                    os.rename(path + '/' + file_name, self.DIRS['train'] + '/' + file_name)
+                    os.rename(path + '/' + file_name, self.TRAIN_DIR + '/' + file_name)
                 elif i < 9:
-                    os.rename(path + '/' + file_name, self.DIRS['valid'] + '/' + file_name)
+                    os.rename(path + '/' + file_name, self.VALID_DIR + '/' + file_name)
                 elif i == 9:
-                    os.rename(path + '/' + file_name, self.DIRS['test'] + '/' + file_name)
+                    os.rename(path + '/' + file_name, self.TEST_DIR + '/' + file_name)
                     i = 0
-        shutil.rmtree(self.SMALL_FILENAME[:-4])
+        shutil.rmtree(self.SMALL_DIR)
 
 
 if __name__ == "__main__":
