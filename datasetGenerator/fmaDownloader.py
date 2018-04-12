82659b7cf31e60f5ba68078146b0050a13a7ed05
amarafioti <andimarafioti@gmail.com>
2018-04-12 10:58:50 +0200

    Called small dir by it's name

--- datasetGenerator/fmaDownloader.py
+++ datasetGenerator/fmaDownloader.py
@@ -8,6 +8,7 @@ __author__ = 'Andres'
 class FMADownloader(Downloader):
     SMALL_LINK = 'https://os.unil.cloud.switch.ch/fma/fma_small.zip'
     SMALL_FILENAME = 'fma_small.zip'
+    SMALL_DIR = SMALL_FILENAME[:-4]
     DIRS = {'train': 'FMA-train', 'valid': 'FMA-valid', 'test': 'FMA-test'}
 
     def _downloadLinksAndFilenames(self):
@@ -25,7 +26,7 @@ class FMADownloader(Downloader):
                 print('Directory already existed, proceed with caution.\nException:', e)
 
         i = 0
-        for path, directory_name, file_names in os.walk(self.SMALL_FILENAME[:-4]):
+        for path, directory_name, file_names in os.walk(self.SMALL_DIR):
             for file_name in file_names:
                 i += 1
                 if i < 7:
