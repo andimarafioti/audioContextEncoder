a889ae5d53eae488752a72f6ce0712829ce93015
amarafioti <andimarafioti@gmail.com>
2018-04-16 14:01:41 +0200

    Params for a convolutional network

---
+++ network/architecture/convNetworkParams.py
@@ -0,0 +1,36 @@
+__author__ = 'Andres'
+
+
+class ConvNetworkParams(object):
+    def __init__(self, filterShapes, channels, strides, name):
+        self._filterShapes = filterShapes
+        self._channels = channels
+        self._strides = strides
+        self._name = name
+
+    def inputShape(self):
+        return self._inputShape
+
+    def filterShapes(self):
+        return self._filterShapes
+
+    def channels(self):
+        return self._channels
+
+    def inputChannels(self):
+        return self._channels[:-1]
+
+    def outputChannels(self):
+        return self._channels[1:]
+
+    def strides(self):
+        return self._strides
+
+    def name(self):
+        return self._name
+
+    def layerCount(self):
+        return len(self._strides)
+
+    def convNames(self):
+        return ["Conv_"+str(index) for index in range(self.layerCount())]
