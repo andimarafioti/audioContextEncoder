2cdc779c4615dcbdd04d639d5ad4a981a8718a19
amarafioti <andimarafioti@gmail.com>
2018-04-19 15:12:06 +0200

    Use saved parameters

--- paperArchitecture.py
+++ paperArchitecture.py
@@ -1,47 +1,14 @@
 import pickle
 
 from architecture.contextEncoderArchitecture import ContextEncoderArchitecture
-from architecture.convNetworkParams import ConvNetworkParams
-from architecture.fullyLayerParams import FullyLayerParams
 from system.contextEncoderSystem import ContextEncoderSystem
 from system.preAndPostProcessor import PreAndPostProcessor
 
-batchSize = 256
-signalLength = 5120
-gapLength = 1024
-fftWindowLength = 512
-fftHopSize = 128
+with open('Context_Encoder_parameters.pkl', 'rb') as savedFile:
+    Context_Encoder_parameters = pickle.load(savedFile)
 
-encoderParams = ConvNetworkParams(filterShapes=[(7, 89), (3, 17), (2, 11),
-                                                (1, 9), (1, 5), (2, 5)],
-                                  channels=[4, 32, 128, 512,
-                                            256, 160, 128],
-                                  strides=[[1, 2, 2, 1], [1, 2, 3, 1], [1, 2, 3, 1],
-                                           [1, 1, 2, 1], [1, 1, 1, 1], [1, 1, 1, 1]],
-                                  name='Encoder')
-
-fullyParams = FullyLayerParams(inputShape=(batchSize, 128, 2, 8), outputShape=(batchSize, 8, 8, 32), name="Fully")
-
-decoderParams = ConvNetworkParams(filterShapes=[(8, 8), (5, 5), (3, 3), (5, 67), (11, 257)],
-                                  channels=[32, 128, 512, 257, 11, 2],
-                                  strides=[[1, 2, 2, 1], [1, 2, 2, 1], [1, 1, 1, 1],
-                                           [1, 2, 2, 1], [1, 1, 1, 1]],
-                                  name='Decoder')
-
-inputShape = (batchSize, 16, 257, 4)
-aContextEncoderArchitecture = ContextEncoderArchitecture(inputShape, encoderParams, decoderParams, fullyParams)
-aPreProcessor = PreAndPostProcessor(signalLength=signalLength, gapLength=gapLength,
-                                    fftWindowLength=fftWindowLength, fftHopSize=fftHopSize)
-
-name = "Context_Encoder"
-aContextEncoderSystem = ContextEncoderSystem(aContextEncoderArchitecture, batchSize, aPreProcessor, name)
-
-dictToSave = {"Architecture Params": [inputShape, encoderParams, decoderParams, fullyParams],
-              "PreProcessor Params": [signalLength, gapLength, fftWindowLength, fftHopSize],
-              "modelName": name,
-              "batchSize": batchSize}
-
-with open(name + '_parameters.pkl', 'wb') as saveFile:
-    pickle.dump(dictToSave, saveFile)
-
+aContextEncoderArchitecture = ContextEncoderArchitecture(*Context_Encoder_parameters['Architecture Params'])
+aPreProcessor = PreAndPostProcessor(*Context_Encoder_parameters['PreProcessor Params'])
+aContextEncoderSystem = ContextEncoderSystem(aContextEncoderArchitecture, Context_Encoder_parameters['batchSize'],
+                                             aPreProcessor, Context_Encoder_parameters['modelName'])
 aContextEncoderSystem.train("nsynth_train_w5120_g1024_h512.tfrecords", "nsynth_valid_w5120_g1024_h512.tfrecords", 1e-3)
