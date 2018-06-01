1454cea69276c89c83ba5386f6cb9d8d56d0670d
andimarafioti <andimarafioti@gmail.com>
2018-06-01 13:58:18 +0200

    add spectrograms

--- system/contextEncoderSystem.py
+++ system/contextEncoderSystem.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from system.dnnSystem import DNNSystem
+from utils.colorize import colorize
 from utils.strechableNumpyArray import StrechableNumpyArray
 from utils.tfReader import TFReader
 
@@ -84,7 +85,8 @@ class ContextEncoderSystem(DNNSystem):
 
     def _evaluationSummaries(self):
         summaries_dict = {'train_SNR_summary': tf.summary.scalar("training_SNR", self._SNR),
-                          'valid_SNR_summary': tf.summary.scalar("validation_SNR", self._SNR)}
+                          'valid_SNR_summary': tf.summary.scalar("validation_SNR", self._SNR),
+    			          'image_summaries': self._spectrogramImageSummary()}
         return summaries_dict
 
     def _squaredEuclideanNorm(self, tensor, onAxis=[1, 2, 3]):
@@ -101,3 +103,23 @@ class ContextEncoderSystem(DNNSystem):
         norm_y_orig = self._squaredEuclideanNorm(y_orig, onAxis)
         norm_y_orig_minus_y_inp = self._squaredEuclideanNorm(y_orig - y_inp, onAxis)
         return 10 * self._log10(norm_y_orig / norm_y_orig_minus_y_inp)
+
+    def _spectrogramImageSummary(self):
+        complexOutput = self._architecture.output()[0]
+        outputSpectrogram = tf.sqrt(tf.square(complexOutput[:, :, 0]) + tf.square(complexOutput[:, :, 1]))
+
+        complexTarget = self._architecture.target()[0]
+        targetSpectrogram = tf.sqrt(tf.square(complexTarget[:, :, 0]) + tf.square(complexTarget[:, :, 1]))
+
+        complexLeft = self._architecture.input()[0, :, :, 0:2]
+        leftSpectrogram = tf.sqrt(tf.square(complexLeft[:, :, 0]) + tf.square(complexLeft[:, :, 1]))
+
+        complexRight = self._architecture.input()[0, :, :, 2:4]
+        rightSpectrogram = tf.sqrt(tf.square(complexRight[:, :, 0]) + tf.square(complexRight[:, :, 1]))
+
+        totalSpectrogram = tf.transpose(tf.concat([leftSpectrogram, outputSpectrogram,
+                                                   rightSpectrogram], axis=0))
+
+        return tf.summary.merge([tf.summary.image("Original", [colorize(tf.transpose(targetSpectrogram))]),
+                                tf.summary.image("Generated", [colorize(tf.transpose(outputSpectrogram))]),
+                                tf.summary.image("Complete", [colorize(totalSpectrogram)])])
