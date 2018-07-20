9e5abb136e42b7c8a24fe3100287fba954a67056
amarafioti <andimarafioti@gmail.com>
2018-07-20 11:33:21 +0200

    spectrograms implemented in a more versatile way to work with magnitude net

--- system/contextEncoderSystem.py
+++ system/contextEncoderSystem.py
@@ -145,16 +145,16 @@ class ContextEncoderSystem(DNNSystem):
 
     def _spectrogramImageSummary(self):
         complexOutput = self._architecture.output()[0]
-        outputSpectrogram = tf.sqrt(tf.square(complexOutput[:, :, 0]) + tf.square(complexOutput[:, :, 1]))
+        outputSpectrogram = tf.sqrt(tf.reduce_sum(tf.square(complexOutput), axis=-1))
 
         complexTarget = self._architecture.target()[0]
-        targetSpectrogram = tf.sqrt(tf.square(complexTarget[:, :, 0]) + tf.square(complexTarget[:, :, 1]))
+        targetSpectrogram = tf.sqrt(tf.reduce_sum(tf.square(complexTarget), axis=-1))
 
         complexLeft = self._architecture.input()[0, :, :, 0:2]
-        leftSpectrogram = tf.sqrt(tf.square(complexLeft[:, :, 0]) + tf.square(complexLeft[:, :, 1]))
+        leftSpectrogram = tf.sqrt(tf.reduce_sum(tf.square(complexLeft), axis=-1))
 
         complexRight = self._architecture.input()[0, :, :, 2:4]
-        rightSpectrogram = tf.sqrt(tf.square(complexRight[:, :, 0]) + tf.square(complexRight[:, :, 1]))
+        rightSpectrogram = tf.sqrt(tf.reduce_sum(tf.square(complexRight), axis=-1))
 
         totalSpectrogram = tf.transpose(tf.concat([leftSpectrogram, outputSpectrogram,
                                                    rightSpectrogram], axis=0))
