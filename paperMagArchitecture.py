43fa7768f914a23a3fb316f78a3e3cc9c3f323b5
amarafioti <andimarafioti@gmail.com>
2018-07-20 11:39:39 +0200

    Needed to use the other pre and post processor

--- paperMagArchitecture.py
+++ paperMagArchitecture.py
@@ -2,7 +2,7 @@ import pickle
 
 from architecture.contextEncoderArchitecture import ContextEncoderArchitecture
 from system.contextEncoderSystem import ContextEncoderSystem
-from system.preAndPostProcessor import PreAndPostProcessor
+from system.magPreAndPostProcessor import MagPreAndPostProcessor
 
 architecturesParametersFile = "Papers_Mag_Context_Encoder_parameters.pkl"
 sessionsName = "Papers_Mag_Context_Encoder"
@@ -11,7 +11,7 @@ with open(architecturesParametersFile, 'rb') as savedFile:
     Context_Encoder_parameters = pickle.load(savedFile)
 
 aContextEncoderArchitecture = ContextEncoderArchitecture(*Context_Encoder_parameters.architectureParameters())
-aPreProcessor = PreAndPostProcessor(*Context_Encoder_parameters.preProcessorParameters())
+aPreProcessor = MagPreAndPostProcessor(*Context_Encoder_parameters.preProcessorParameters())
 aContextEncoderSystem = ContextEncoderSystem(aContextEncoderArchitecture, Context_Encoder_parameters.batchSize(),
                                              aPreProcessor, sessionsName)
 aContextEncoderSystem.train("nsynth_train_w5120_g1024_h512.tfrecords", "nsynth_valid_w5120_g1024_h512.tfrecords", 1e-3)
